98c7c13344004d8f7b4230bdbb27fe35f9a1f60e
Your Name <you@example.com>
2023-04-12 12:00:58 +0300

    user module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {UserModule} from "./user/user.module";
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
+    UserModule,
     AppRoutingModule
   ],
   providers: [],
