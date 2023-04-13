79cad50bdfa4a11e486300f384a2d6f532d06e7d
Aleksandr Seppenen <alex.seppenen@gmail.com>
2023-04-13 18:19:57 +0300

    commit

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,31 @@
+import {AfterContentInit, Component, ContentChildren, QueryList} from '@angular/core';
+import {TabComponent} from "../tab/tab.component";
+
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList<TabComponent>();
+
+
+  constructor() {
+  }
+
+
+  ngAfterContentInit() {
+    console.log(this.tabs);
+    const activeTabs = this.tabs.filter((tab) => tab.active);
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+
+  selectTab(first: TabComponent) {
+    this.tabs.forEach(tab => tab.active = false);
+    first.active = true;
+  }
+}
